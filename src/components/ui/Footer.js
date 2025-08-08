e12e48c53d247e31f3cad87a8b5bdb66862150aa
kang`ethe <offline@kali.dkmuthunga.com>
2025-08-08 16:36:17 +0300

    the footer updated

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -13,353 +13,111 @@ import instagram from "../../assets/instagram.svg";
 
 const useStyles = makeStyles((theme) => ({
   footer: {
-    backgroundColor: theme.palette.common.orange,
+    backgroundColor: theme.palette.primary.dark,
+    color: "white",
     width: "100%",
-    overflowX: "hidden",
+    paddingTop: "4rem",
+    paddingBottom: "2rem",
+    position: "relative",
     zIndex: theme.zIndex.modal + 1,
-    [theme.breakpoints.down("sm")]: {
-      backgroundColor: "black",
-      height: "7.5em",
-      overflowX: "visible",
-    },
   },
   adornment: {
-    width: "21em",
-    verticalAlign: "bottom",
-    overflowY: "visible",
-    [theme.breakpoints.down("md")]: {
-      width: "20em",
-    },
-    [theme.breakpoints.down("sm")]: {
-      position: "absolute",
-      zIndex: 1302,
-      width: "100%",
-    },
+    width: "100%",
+    marginTop: "2rem",
   },
   mainContainer: {
-    position: "absolute",
-    marginTop: "auto",
-    marginRight: 0,
     width: "100%",
-    overflow: "hidden",
-    paddingLeft: "2rem",
-    [theme.breakpoints.down("md")]: {
-      marginTop: "0.5rem",
-      justifyContentContent: "flex-end",
-      paddingRight: "9%",
-      margin: 0,
-    },
+    maxWidth: "1200px",
+    margin: "auto",
+    padding: "0 2rem",
   },
   link: {
     ...theme.typography.link,
-    opacity: 0.7,
+    color: "white",
     textDecoration: "none",
-    cursor: "pointer",
-    whiteSpace: "nowrap",
+    opacity: 0.8,
+    marginBottom: "0.5rem",
+    transition: "opacity 0.3s ease",
     "&:hover": {
       opacity: 1,
     },
   },
+  sectionTitle: {
+    marginBottom: "1rem",
+    fontWeight: 600,
+    fontSize: "1.2rem",
+  },
   icon: {
-    width: "3.5rem",
-    height: "3.5rem",
-    padding: "0.5rem",
-    [theme.breakpoints.down("sm")]: {
-      width: "7rem",
-      height: "7rem",
-      padding: "1rem",
-    },
-    [theme.breakpoints.down("xs")]: {
-      width: "6rem",
-      height: "6rem",
+    width: "2rem",
+    height: "2rem",
+    marginRight: "1rem",
+    transition: "transform 0.3s ease",
+    "&:hover": {
+      transform: "scale(1.1)",
     },
   },
   mediaIconsContainer: {
-    marginTop: "-3.5rem",
-    paddingRight: "2rem",
-    margin: 0,
-    [theme.breakpoints.down("sm")]: {
-      margin: "auto",
-      padding: "1.5rem",
-      position: "absolute",
-      zIndex: 1303,
-    },
-    [theme.breakpoints.down("xs")]: {
-      paddingTop: "0.5rem",
-      padding: 0,
-    },
+    marginTop: "2rem",
+    justifyContent: "center",
+  },
+  copyright: {
+    marginTop: "2rem",
+    fontSize: "0.875rem",
+    opacity: 0.6,
+    textAlign: "center",
   },
 }));
 
 const Footer = (props) => {
   const classes = useStyles();
-  const theme = useTheme();
-  const medium = useMediaQuery(theme.breakpoints.down("md"));
-  const smaller = useMediaQuery(theme.breakpoints.down("sm"));
+  // const theme = useTheme();
+  // const medium = useMediaQuery(theme.breakpoints.down("md"));
+  // const smaller = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
-    <Grid container className={classes.footer}>
-      <Hidden smDown>
-        <Grid
-          item
-          container
-          justifyContent="center"
-          className={classes.mainContainer}
-          spacing={medium ? 5 : 10}
-        >
-          <Grid item>
-            <Grid container direction="column" spacing={2}>
-              <Grid
-                item
-                className={classes.link}
-                style={
-                  window.matchMedia("(max-width: 1450px)").matches
-                    ? { display: "none" }
-                    : {}
-                }
-                onClick={() => {
-                  props.setValue(0);
-                  props.setSelected(null);
-                }}
-                component={Link}
-                to="/"
-              >
-                Home
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <Grid container direction="column" spacing={2}>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(1);
-                  props.setSelected(null);
-                }}
-                component={Link}
-                to="/services"
-              >
-                Services
-              </Grid>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(1);
-                  props.setSelected(0);
-                }}
-                component={Link}
-                to="/software"
-              >
-                Software Development
-              </Grid>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(1);
-                  props.setSelected(1);
-                }}
-                component={Link}
-                to="/mobileapps"
-              >
-                App Development
-              </Grid>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(1);
-                  props.setSelected(2);
-                }}
-                component={Link}
-                to="/websites"
-              >
-                Web Development
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <Grid container direction="column" spacing={2}>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(2);
-                  props.setSelected(null);
-                }}
-                component={Link}
-                to="/revolution"
-              >
-                Revolution
-              </Grid>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(2);
-                  props.setSelected(null);
-                }}
-                component={Link}
-                to="/revolution"
-              >
-                Vision
-              </Grid>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(2);
-                  props.setSelected(null);
-                }}
-                component={Link}
-                to="/revolution"
-              >
-                Technology
-              </Grid>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(2);
-                  props.setSelected(null);
-                }}
-                component={Link}
-                to="/revolution"
-              >
-                Process
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <Grid container direction="column" spacing={2}>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(3);
-                  props.setSelected(null);
-                }}
-                component={Link}
-                to="/about"
-              >
-                About Us
-              </Grid>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(3);
-                  props.setSelected(null);
-                }}
-                component={Link}
-                to="/about"
-              >
-                History
-              </Grid>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(3);
-                  props.setSelected(null);
-                }}
-                component={Link}
-                to="/about"
-              >
-                Team
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <Grid container direction="column" spacing={2}>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(4);
-                  props.setSelected(null);
-                }}
-                component={Link}
-                to="/contact"
-              >
-                Contact Us
-              </Grid>
-              <Grid
-                item
-                className={classes.link}
-                onClick={() => {
-                  props.setValue(5);
-                  props.setSelected(null);
-                }}
-                component={Link}
-                to="/estimate"
-              >
-                Get Estimate
-              </Grid>
-            </Grid>
-          </Grid>
+    <footer className={classes.footer}>
+      <Grid container spacing={4} className={classes.mainContainer}>
+        <Grid item xs={6} sm={3}>
+          <div className={classes.sectionTitle}>Company</div>
+          <div className={classes.link} component={Link} to="/" onClick={() => props.setValue(0)}>Home</div>
+          <div className={classes.link} component={Link} to="/about" onClick={() => props.setValue(3)}>About Us</div>
+          <div className={classes.link} component={Link} to="/contact" onClick={() => props.setValue(4)}>Contact Us</div>
         </Grid>
-      </Hidden>
-      {smaller ? null : (
-        <Grid item className={classes.adornment}>
-          <FooterAdornment className={classes.adornment} />
+        <Grid item xs={6} sm={3}>
+          <div className={classes.sectionTitle}>Services</div>
+          <div className={classes.link} component={Link} to="/software" onClick={() => props.setValue(1)}>Software</div>
+          <div className={classes.link} component={Link} to="/mobileapps" onClick={() => props.setValue(1)}>Apps</div>
+          <div className={classes.link} component={Link} to="/websites" onClick={() => props.setValue(1)}>Websites</div>
         </Grid>
-      )}
-      <Grid container direction="column" alignItems="center">
-        <Grid
-          item
-          container
-          justifyContent={smaller ? "center" : "flex-end"}
-          className={classes.mediaIconsContainer}
-        >
-          <Grid
-            item
-            component={"a"}
-            href="https://facebook.com"
-            className={classes.icon}
-          >
-            <img
-              alt="facebook-icon"
-              src={facebook}
-              rel="noopener noreferrer"
-              target="_blank"
-            />
-          </Grid>
-          <Grid
-            item
-            component={"a"}
-            href="https://instagram.com"
-            className={classes.icon}
-          >
-            <img
-              alt="facebook-icon"
-              src={instagram}
-              rel="noopener noreferrer"
-              target="_blank"
-            />
-          </Grid>
-          <Grid
-            item
-            component={"a"}
-            href="https://twitter.com"
-            className={classes.icon}
-          >
-            <img
-              alt="facebook-icon"
-              src={twitter}
-              rel="noopener noreferrer"
-              target="_blank"
-            />
-          </Grid>
+        <Grid item xs={6} sm={3}>
+          <div className={classes.sectionTitle}>Explore</div>
+          <div className={classes.link} component={Link} to="/revolution" onClick={() => props.setValue(2)}>Vision</div>
+          <div className={classes.link} component={Link} to="/revolution" onClick={() => props.setValue(2)}>Technology</div>
+          <div className={classes.link} component={Link} to="/revolution" onClick={() => props.setValue(2)}>Process</div>
         </Grid>
-        {smaller ? (
-          <Grid item className={classes.adornment}>
-            <MobileFooterAdornment />
-          </Grid>
-        ) : null}
+        <Grid item xs={6} sm={3}>
+          <div className={classes.sectionTitle}>Support</div>
+          <div className={classes.link} component={Link} to="/estimate" onClick={() => props.setValue(5)}>Get Estimate</div>
+        </Grid>
       </Grid>
-    </Grid>
+      <Grid container className={classes.mediaIconsContainer}>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <img src={facebook} alt="Facebook" className={classes.icon} />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <img src={instagram} alt="Instagram" className={classes.icon} />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <img src={twitter} alt="Twitter" className={classes.icon} />
+        </a>
+      </Grid>
+      {/* <Grid item className={classes.adornment}>
+        {smaller ? <MobileFooterAdornment /> : <FooterAdornment />}
+      </Grid> */}
+      <div className={classes.copyright}>
+        &copy; {new Date().getFullYear()} dancah Technology. All rights reserved.
+      </div>
+    </footer>
   );
 };
 
